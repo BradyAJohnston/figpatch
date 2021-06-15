b5c8ec91918709d159efab368912dd745ab31bd5
Brady <brady.johnston@me.com>
2021-06-15 17:08:44 +0800

    add minor tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(figpatch)
+
+test_check("figpatch")
---
+++ tests/testthat/test-fig.R
@@ -0,0 +1,14 @@
+test_that("Test Reading", {
+  expect_error(
+    # fail while reading in rubbish link
+    fig("brokenlink"), {
+    "No such file or directory"
+  })
+  expect_silent({
+    # read in without trouble
+    temp <- fig(
+      system.file("extdata", "fig.png", package = "figpatch", mustWork = TRUE)
+    )
+  }
+  )
+})
